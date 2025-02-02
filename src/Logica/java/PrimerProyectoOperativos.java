cb84c91be00e3dcf87855a12ec8f3f9d45be78a6
CBastidas707 <c.bastidas@correo.unimet.edu.ve>
2025-02-02 18:29:24 -0400

    Se agregó la estructura lista y se adaptó a una clase básica proceso

--- src/Logica/java/PrimerProyectoOperativos.java
+++ src/Logica/java/PrimerProyectoOperativos.java
@@ -1,4 +1,7 @@
 package Logica.java;
+import Logica.java.Estructuras.List;
+import Logica.java.Estructuras.Nodo;
+import Logica.java.Estructuras.Proceso;
 
 public class PrimerProyectoOperativos {
 
@@ -6,7 +9,26 @@ public class PrimerProyectoOperativos {
     
     public static void main(String[] args) {
         
+       /* Esto es una prueba para crear listas y verificar que sirve
+       List lista = new List("Prueba");
+       
+        Proceso proceso1 = new Proceso("Proceso1", 5);
+        Proceso proceso2 = new Proceso("Proceso2", 4);
+        Proceso proceso3 = new Proceso("Proceso3", 3);
+        Proceso proceso4 = new Proceso("Proceso4", 2);
+        Proceso proceso5 = new Proceso("Proceso5", 1);
+        Proceso proceso45 = new Proceso("Proceso4,5", 0);
         
+        
+        lista.insertFirst(proceso1);
+        lista.insertFirst(proceso2);
+        lista.insertFirst(proceso3);
+        lista.insertFirst(proceso4);
+        lista.insertFirst(proceso5);
+        
+        lista.insert(proceso45,lista.find("Proceso5"));
+        
+        System.out.println(lista.travel());   */
         
         
     }
