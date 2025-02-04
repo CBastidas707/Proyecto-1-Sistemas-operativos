5f121066d5e7addec2762e5aaf8c764d5b757703
CBastidas707 <c.bastidas@correo.unimet.edu.ve>
2025-02-04 08:56:06 -0400

    Se creó una clase adicional proceso y se diferenció de la clase que estaba previamente. La que estaba antes se convirtió en Imagen de proceso (que se usa sólo para cuando se crea el proceso, es la información base del proceso cuando se creó) y la nueva es el proceso que se ejecutará recibiendo su PCB

--- src/Logica/java/Process.java
+++ src/Logica/java/Process.java
@@ -1,76 +1,40 @@
 package Logica.java;
 
-public class Process {
-    private String process_name;
-    private int process_length; // Cantidad de instrucciones
-    public String consumption_category;  // Si es CPU Bound o I/O Bound
-    
-    // Parámetros de procesos I/O Bound
-    public int exceptionRequirement = 0;     // ¿Cuántos ciclos se requieren para generar una excepción?
-    public int exceptionCoverage = 0;        // ¿Cuántos ciclos se requieren para satisfacer una excepción?
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-    // Constructor proceso CPU Bound
-    public Process(String process_name, int process_length) {
-        this.process_name = process_name;
-        this.process_length = process_length;
-        this.consumption_category = "CPU Bound";
-    }
+/**
+ *
+ * @author carlo_7ogoiii
+ */
+public class Process extends Thread{
     
-    // Constructor proceso I/O Bound
-    
-        public Process(String process_name, int process_length, int exceptionRequirement, int exceptionCoverage) {
-        this.process_name = process_name;
-        this.process_length = process_length;
-        this.consumption_category = "I/O Bound";
-        this.exceptionRequirement = exceptionRequirement;
-        this.exceptionCoverage = exceptionCoverage;
+    private PCB pcb;
+
+    public Process(PCB pcb) {
+        this.pcb = pcb;
     }
     
-
-    // Nombre del Proceso
-    public String getProcess_name() {
-        return process_name;
-    }
-
-    public void setProcess_name(String process_name) {
-        this.process_name = process_name;
-    }
-
-    // Cantidad de instrucciones
-    public int getProcess_length() {
-        return process_length;
-    }
-
-    public void setProcess_length(int process_length) {
-        this.process_length = process_length;
-    }
-
-    // Tipo de consumo. DEBE AGREGARSE LA RESTRICCIÓN: I/O Bound / CPU Bound
-    public String getConsumption_category() {
-        return consumption_category;
-    }
-
-    public void setConsumption_category(String consumption_category) {
-        this.consumption_category = consumption_category;
-    }
-
-    // Parámetros para el caso I/O Bound
-    public int getExceptionRequirement() {
-        return exceptionRequirement;
-    }
-
-    public void setExceptionRequirement(int exceptionRequirement) {
-        this.exceptionRequirement = exceptionRequirement;
-    }
-
-    public int getExceptionCoverage() {
-        return exceptionCoverage;
-    }
-
-    public void setExceptionCoverage(int exceptionCoverage) {
-        this.exceptionCoverage = exceptionCoverage;
+    @Override
+    public void run(){
+        while(true){
+         
+            try {
+                System.out.println(
+                        "--------------------------------\n"
+                        + "Proceso: " + pcb.getProcess_name()
+                        + "\n" + "MAR: " + pcb.getMAR_Status()
+                        + "\n" + "PC: " + pcb.getPC_Status()
+                        + "\n" + "Longitud: " + pcb.getLength()
+                        + "\n--------------------------------\n");
+                pcb.setMAR_Status(pcb.getMAR_Status() + 1);
+                pcb.setPC_Status(pcb.getPC_Status() + 1);
+                sleep(1000);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Process.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
     
     
 }
-    
---
+++ src/Logica/java/Process_Image.java
@@ -0,0 +1,90 @@
+package Logica.java;
+
+public class Process_Image {
+    private String process_name;
+    private int process_length; // Cantidad de instrucciones
+    public String consumption_category;  // Si es CPU Bound o I/O Bound
+    private PCB pcb;
+    
+    // Parámetros de procesos I/O Bound
+    public int exceptionRequirement = 0;     // ¿Cuántos ciclos se requieren para generar una excepción?
+    public int exceptionCoverage = 0;        // ¿Cuántos ciclos se requieren para satisfacer una excepción?
+
+    // Constructor proceso CPU Bound
+    public Process_Image(String process_name, int process_length) {
+        PCB newpcb = new PCB(process_name, process_length, "Ready");
+        this.process_name = process_name;
+        this.process_length = process_length;
+        this.consumption_category = "CPU Bound";
+        this.pcb = newpcb;
+    }
+    
+    // Constructor proceso I/O Bound
+    
+        public Process_Image(String process_name, int process_length, int exceptionRequirement, int exceptionCoverage) {
+        PCB newpcb = new PCB(process_name, process_length, "Ready");
+        this.process_name = process_name;
+        this.process_length = process_length;
+        this.consumption_category = "I/O Bound";
+        this.exceptionRequirement = exceptionRequirement;
+        this.exceptionCoverage = exceptionCoverage;
+        this.pcb = newpcb;
+    }
+    
+
+    // Nombre del Proceso
+    public String getProcess_name() {
+        return process_name;
+    }
+
+    public void setProcess_name(String process_name) {
+        this.process_name = process_name;
+    }
+
+    // Cantidad de instrucciones
+    public int getProcess_length() {
+        return process_length;
+    }
+
+    public void setProcess_length(int process_length) {
+        this.process_length = process_length;
+    }
+
+    // Tipo de consumo. DEBE AGREGARSE LA RESTRICCIÓN: I/O Bound / CPU Bound
+    public String getConsumption_category() {
+        return consumption_category;
+    }
+
+    public void setConsumption_category(String consumption_category) {
+        this.consumption_category = consumption_category;
+    }
+
+    // Parámetros para el caso I/O Bound
+    public int getExceptionRequirement() {
+        return exceptionRequirement;
+    }
+
+    public void setExceptionRequirement(int exceptionRequirement) {
+        this.exceptionRequirement = exceptionRequirement;
+    }
+
+    public int getExceptionCoverage() {
+        return exceptionCoverage;
+    }
+
+    public void setExceptionCoverage(int exceptionCoverage) {
+        this.exceptionCoverage = exceptionCoverage;
+    }
+
+    public PCB getPcb() {
+        return pcb;
+    }
+
+    public void setPcb(PCB pcb) {
+        this.pcb = pcb;
+    }
+    
+    
+    
+}
+    
