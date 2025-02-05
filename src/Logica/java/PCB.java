20a4fa6f001d541e7b6c60c076f2fa149d8a92b9
CBastidas707 <c.bastidas@correo.unimet.edu.ve>
2025-02-05 16:49:08 -0400

    Se agregó funcionalidad para definir el tiempo de ejecucion de las instruciones

--- src/Logica/java/PCB.java
+++ src/Logica/java/PCB.java
@@ -6,13 +6,29 @@ public class PCB {
     private int Length;
     private int PC_Status;
     private int MAR_Status;
+    private int exceptionG = 0;     // ¿Cuántos ciclos se requieren para generar una excepción?
+    private int exceptionD = 0;     // Ciclos para satisfacerla
 
+    // CPU Bound
+    
     public PCB(String Process_name, int length, String status) {
         this.Process_name = Process_name;
         this.Length = length;
         this.status = status;
         this.PC_Status = 1;
         this.MAR_Status = 0;
+    }
+    
+    // I/O Bound
+    
+    public PCB(String Process_name, int length, String status, int exceptionG, int exceptionD) {
+        this.Process_name = Process_name;
+        this.Length = length;
+        this.status = status;
+        this.PC_Status = 1;
+        this.MAR_Status = 0;
+        this.exceptionG = exceptionG;
+        this.exceptionD = exceptionD;
     }
 
     // ID del proceso
@@ -58,6 +74,24 @@ public class PCB {
     public void setLength(int Length) {
         this.Length = Length;
     }
+
+    public int getExceptionG() {
+        return exceptionG;
+    }
+
+    public void setExceptionG(int exceptionG) {
+        this.exceptionG = exceptionG;
+    }
+
+    public int getExceptionD() {
+        return exceptionD;
+    }
+
+    public void setExceptionD(int exceptionD) {
+        this.exceptionD = exceptionD;
+    }
+    
+    
     
     
 }
--- src/Logica/java/Process_Image.java
+++ src/Logica/java/Process_Image.java
@@ -22,7 +22,7 @@ public class Process_Image {
     // Constructor proceso I/O Bound
     
         public Process_Image(String process_name, int process_length, int exceptionRequirement, int exceptionCoverage) {
-        PCB newpcb = new PCB(process_name, process_length, "Ready");
+        PCB newpcb = new PCB(process_name, process_length, "Ready", exceptionRequirement, exceptionCoverage);
         this.process_name = process_name;
         this.process_length = process_length;
         this.consumption_category = "I/O Bound";
